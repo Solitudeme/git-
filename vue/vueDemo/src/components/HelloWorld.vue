c870ed5c736f2947bd21f832f26c442c82d63547
cjc <1171317712@qq.com>
2018-11-05 17:44:58 +0800

    新增vue demo项目文件

---
+++ vue/vueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2 v-if="bool">Essential Links {{ num }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      num: '1000',
+      bool: false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+*{
+  padding:0;margin:0;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
